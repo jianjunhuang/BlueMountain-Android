3cbc8b0b0f8a235b374a5617331eaf27c31e9790
jianjunhuang <demon903732218@gmail.com>
2018-04-26 10:51:36 +0800

    add lock

--- app/src/main/java/com/jianjunhuang/bluemountain/application/UserInfo.java
+++ app/src/main/java/com/jianjunhuang/bluemountain/application/UserInfo.java
@@ -9,42 +9,53 @@ public class UserInfo {
 
     private static User user;
     private static Machine machine;
+    private static final Object lock = new Object();
 
-    public static synchronized User getUser() {
-        if (user == null) {
-            String userJson = SPUtils.instance(Config.SP_USER_INFO_FILE).getInfo(Config.SP_USER_INFO_KEY, "");
-            if (userJson.equals("")) {
-                return null;
+    public static User getUser() {
+        synchronized (lock) {
+            if (user == null) {
+                String userJson = SPUtils.instance(Config.SP_USER_INFO_FILE).getInfo(Config.SP_USER_INFO_KEY, "");
+                if (userJson.equals("")) {
+                    return null;
+                }
+                user = new Gson().fromJson(userJson, User.class);
             }
-            user = new Gson().fromJson(userJson, User.class);
+            return user;
         }
-        return user;
     }
 
-    public static synchronized Machine getMachine() {
-        if (machine == null) {
-            String machineJson = SPUtils.instance(Config.SP_MACHINE_INFO_FILE).getInfo(Config.SP_MACHINE_INFO_KEY, "");
-            if (machineJson.equals("")) {
-                return null;
+    public static Machine getMachine() {
+        synchronized (lock) {
+            if (machine == null) {
+                String machineJson = SPUtils.instance(Config.SP_MACHINE_INFO_FILE).getInfo(Config.SP_MACHINE_INFO_KEY, "");
+                if (machineJson.equals("")) {
+                    return null;
+                }
+                machine = new Gson().fromJson(machineJson, Machine.class);
             }
-            machine = new Gson().fromJson(machineJson, Machine.class);
+            return machine;
         }
-        return machine;
     }
 
     public static void setUser(User user) {
-        UserInfo.user = user;
-        if (null != user) {
-            SPUtils.instance(Config.SP_USER_INFO_FILE)
-                    .store(Config.SP_USER_INFO_KEY, new Gson().toJson(user));
+        synchronized (lock) {
+            UserInfo.user = user;
+            if (null != user) {
+                Gson gson = new Gson();
+                SPUtils.instance(Config.SP_USER_INFO_FILE)
+                        .store(Config.SP_USER_INFO_KEY, gson.toJson(user));
+            }
         }
     }
 
     public static void setMachine(Machine machine) {
-        UserInfo.machine = machine;
-        if (null != machine) {
-            SPUtils.instance(Config.SP_MACHINE_INFO_FILE)
-                    .store(Config.SP_MACHINE_INFO_KEY, new Gson().toJson(machine));
+        synchronized (lock) {
+            UserInfo.machine = machine;
+            if (null != machine) {
+                Gson gson = new Gson();
+                SPUtils.instance(Config.SP_MACHINE_INFO_FILE)
+                        .store(Config.SP_MACHINE_INFO_KEY, gson.toJson(machine));
+            }
         }
     }
 }
