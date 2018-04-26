50cf3b453c5bdeb9ea0ea75882f48c3528c9ebf6
jianjunhuang <demon903732218@gmail.com>
2018-04-26 11:51:08 +0800

    add jude parameter job

--- app/src/main/java/com/jianjunhuang/bluemountain/presenter/SignInUpPresenter.java
+++ app/src/main/java/com/jianjunhuang/bluemountain/presenter/SignInUpPresenter.java
@@ -41,16 +41,48 @@ public class SignInUpPresenter implements SignInUpContact.Presenter {
 
     @Override
     public void signIn(String userName, String pwd) {
+        if (null == userName || "".equals(userName)) {
+            mView.onSignInFailed("请输入用户名");
+            return;
+        }
+        if (null == pwd || "".equals(pwd)) {
+            mView.onSignInFailed("请输入密码");
+            return;
+        }
+        if (pwd.length() != 8) {
+            mView.onSignInFailed("请输入 8位 的正确密码");
+            return;
+        }
         mModel.signIn(userName, pwd);
     }
 
     @Override
     public void signUp(String userName, String pwd, String surePwd) {
+        if (null == userName || "".equals(userName)) {
+            mView.onSignUpFailed("请输入用户名");
+            return;
+        }
+        if (null == pwd || "".equals(pwd) || pwd.length() != 8) {
+            mView.onSignUpFailed("请输入 8位 密码");
+            return;
+        }
+        if (null == surePwd || "".equals(surePwd) || surePwd.length() != 8) {
+            mView.onSignUpFailed("请再次输入 8位 密码");
+            return;
+        }
+        if (!pwd.equals(surePwd)) {
+            mView.onSignUpFailed("两次输入的密码不一致，请重新输入");
+            return;
+        }
         mModel.signUp(userName, pwd, surePwd);
     }
 
     @Override
     public void checkUserName(String userName) {
+        if (null == userName || "".equals(userName)) {
+            mView.onUserNameStatus(false);
+            return;
+        }
         mModel.checkUserName(userName);
     }
 
